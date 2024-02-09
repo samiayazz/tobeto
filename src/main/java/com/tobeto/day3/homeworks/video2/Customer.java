c83ee32fd63c086726787dbbbcc974c04ea39f1b
samiayazz <ayazzsami@gmail.com>
2024-02-09 12:11:37 +0300

    Add homework 2 of day 3

---
+++ src/main/java/com/tobeto/day3/homeworks/video2/Customer.java
@@ -0,0 +1,32 @@
+package com.tobeto.day3.homeworks.video2;
+
+class Customer {
+
+    private int id;
+    private String customerNumber;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
